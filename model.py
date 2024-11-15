d5bf0473b3b7ac38cba85b33157b00b15876925a
yangxu3 <yangxu3>
2024-11-15 17:39:11 +0800

    monitor binance

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ class Symbol(Model):
     id = fields.IntField(pk=True)
     symbol = fields.CharField(max_length=100, unique=True)
     status = fields.CharField(max_length=50)
-    baseAsset = fields.CharField(max_length=50, index=True)
-    quoteAsset = fields.CharField(max_length=50)
+    base_asset = fields.CharField(max_length=50, index=True)
+    quote_asset = fields.CharField(max_length=50)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
 
